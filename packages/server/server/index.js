69463896a1b2ff1b2afa0c31f0a61eab077e2851
Kevin <kevin.tang@twosigma.com>
2020-09-12 18:51:22 -0500

    added websocket server and logging

--- packages/server/server/index.js
+++ packages/server/server/index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const WebSocket = require('ws');
+const logger = require('./src/logger');
+
+const wss = new WebSocket.Server({ port: 8080 });
+const clients = [];
+
+wss.on('connection', (ws) => {
+  logger.info('player joined');
+  ws.on('message', (msg) => {
+    logger.info(`message received from player:\n${msg}`);
+  });
+
+  clients.push(ws);
+});
