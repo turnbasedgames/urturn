cc9b1efd6bf83f0d9c0c79e1370ceebaf144d21a
Kevin <kevin.tang@twosigma.com>
2020-09-13 00:11:22 -0500

    basic messaging system

--- packages/server/server/test-client/index.js
+++ packages/server/server/test-client/index.js
@@ -1,11 +1,30 @@
 const WebSocket = require('ws');
+const readline = require('readline');
 const logger = require('../src/logger');
 
 const WS_PATH = 'ws://localhost:8080';
 const ws = new WebSocket(WS_PATH);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.setPrompt('send> ');
+rl.on('line', (line) => {
+  if (line === 'exit') {
+    rl.close();
+  } else {
+    ws.send(line);
+    rl.prompt();
+  }
+}).on('close', () => {
+  logger.info('bye');
+  process.exit(0);
+});
 
 ws.on('open', () => {
   logger.info(`Connection opened with at ${WS_PATH}`);
+  rl.prompt();
 });
 
 ws.on('message', (data) => {
