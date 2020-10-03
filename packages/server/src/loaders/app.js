5b0a7c56a71a08699ffa6d0cea4abcc11583dd49
Kevin <kevin.tang@twosigma.com>
2020-10-03 02:10:02 -0500

    add error Handler

--- packages/server/src/loaders/app.js
+++ packages/server/src/loaders/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { errorHandler } = require('src/api/middleware');
 
 function setup(port, logger) {
   const app = express();
   // TODO: splitup middleware
+  // TODO: get async support with error handling
+  // TODO: setup api logger
+  // TODO: setup celebrate for request verification
+  // TODO: setup authentication for users (JWT Authentication server)
   app.use(bodyParser.urlencoded());
   app.use(bodyParser.json());
 
   // TODO: create routers
+
+  app.use(errorHandler);
+
   const server = app.listen(port, () => {
     logger.info(`App listening on ${server.address().port}`);
   });
