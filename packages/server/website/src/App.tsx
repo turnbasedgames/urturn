5377c7785891d70a3406d4a789a151f9d30aa178
Kevin Tang <kevin.tang@twosigma.com>
2020-12-05 22:01:16 -0600

    material ui app bar

--- packages/server/website/src/App.tsx
+++ packages/server/website/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code> src/App.tsx </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>Hello</Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
