1e4273a1d266ba8b53f317f8e4a82a4c86ec61bb
Kevin Tang <kevin.tang@twosigma.com>
2021-03-14 16:23:52 -0500

    add procfile

--- packages/server/src/middleware/auth.js
+++ packages/server/src/middleware/auth.js
@@ -6,7 +6,9 @@ const logger = require('../logger');
 const User = require('../models/user/user');
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: process.env.GOOGLE_APPLICATION_CREDENTIALS
+    ? admin.credential.applicationDefault()
+    : admin.credential.cert(JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString('ascii'))),
 });
 
 module.exports = asyncHandler(async (req, res, next) => {
