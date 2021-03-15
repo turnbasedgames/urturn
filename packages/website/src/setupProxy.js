a88309dd1aaa6782b5edbbfbf239c2f1372cd407
Kevin Tang <kevin.tang@twosigma.com>
2021-03-14 23:10:35 -0500

    fix lint issues

--- packages/website/src/setupProxy.js
+++ packages/website/src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function setupProxy(app) {
   app.use(
     '/api',
     createProxyMiddleware({
       target: 'http://localhost:8080',
-      pathRewrite: {'^/api': ''}
-    })
+      pathRewrite: { '^/api': '' },
+    }),
   );
 };
