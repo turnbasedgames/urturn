de4436fa55082e0da3a3eac510b14c35022757b1
sarahannali <55365172+sarahannali@users.noreply.github.com>
2021-04-03 16:03:26 -0500

    [MAIN-23] Replace MongoDB-Memory-Server (#15)
    * Remove dependency
    
    * Name replica DB
    
    * Add console.log for mongodb connection string
    
    * Update connection url
    
    * updating connection string
    
    * update connection string
    
    * update yml
    
    * remove mongo connection from app.js
    
    * move mongodb env to app.js
    
    * remove dependency
    
    * update connection url in app.js
    
    * update connection string and replica set name
    
    * add mongodb version to yaml
    
    * update mondogb version
    
    * move start mongodb in yml
    
    * removing repl set"
    
    * adding repl set
    
    * update repl set name
    
    * update connection url
    
    * remove console.log

--- packages/server/test/util/app.js
+++ packages/server/test/util/app.js
@@ -2,18 +2,6 @@ require('dotenv').config();
 const { spawn } = require('child_process');
 const axios = require('axios');
 const getPort = require('get-port');
-const { MongoMemoryReplSet } = require('mongodb-memory-server');
-
-async function spawnDB() {
-  const mongod = new MongoMemoryReplSet();
-  await mongod.waitUntilRunning();
-  const mongoUri = await mongod.getUri();
-  return { mongod, mongoUri };
-}
-
-async function killDB(mongod) {
-  await mongod.stop();
-}
 
 function waitUntilRunning(api, timeout = 10000, buffer = 200) {
   const timeoutThreshold = Date.now() + timeout;
@@ -51,9 +39,8 @@ function killServer(server) {
 }
 
 async function spawnApp() {
-  const { mongod, mongoUri } = await spawnDB();
   const env = {
-    MONGODB_CONNECTION_URL: `${mongoUri}&retryWrites=false`,
+    MONGODB_CONNECTION_URL: 'mongodb://localhost:27017/test?replicaSet=testrs',
     PATH: process.env.PATH,
     PORT: await getPort(),
   };
@@ -65,12 +52,11 @@ async function spawnApp() {
   }
   const api = axios.create({ baseURL: `http://localhost:${env.PORT}` });
   const server = await spawnServer(env, api);
-  return { api, mongod, server };
+  return { api, server };
 }
 
 async function killApp(app) {
   killServer(app.server);
-  await killDB(app.mongod);
 }
 
 module.exports = { spawnApp, killApp };
