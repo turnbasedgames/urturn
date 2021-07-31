1e9697ccb8584c3d27caaf444e8c647f7ca46cbc
Sarah <asarahali00@gmail.com>
2021-07-31 16:36:16 -0500

    first commit

---
+++ packages/runner/index.js
@@ -0,0 +1,9 @@
+// const path = require('path')
+
+// var p = path.join(__dirname, "FrontEnd", "index.html")
+
+// console.log("path: " + p)
+
+// document.getElementById("localFrame").src = p;
+
+console.log("in js")
