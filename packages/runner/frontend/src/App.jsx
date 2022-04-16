737c432a46afc5e61bff05ba727ad23a5744173a
Kevin <kevin26428@gmail.com>
2022-04-16 14:21:33 -0500

    remove unecessary state managed

--- packages/runner/frontend/src/App.jsx
+++ packages/runner/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Stack } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
@@ -6,32 +6,13 @@ import GameManager from './gameManager';
 import Timeline from './timeline';
 
 function App() {
-  const [actionHistory, setActionHistory] = useState([]);
-  const addActionToHistory = (action) => {
-    setActionHistory([...actionHistory, action]);
-  };
-  const [gameStateHistory, setGameStateHistory] = useState([]);
-  const addStateToGameStateHistory = (state) => {
-    if (gameStateHistory.length === 0) {
-      addActionToHistory({ name: 'initialize game' });
-    }
-    setActionHistory([...gameStateHistory, state]);
-  };
-  const resetGame = () => {
-    setActionHistory([]);
-    setGameStateHistory([]);
-  };
   return (
     <ThemeProvider theme={theme}>
       <Stack
         height="100vh"
         overflow="auto"
       >
-        <GameManager
-          addActionToHistory={addActionToHistory}
-          addStateToGameStateHistory={addStateToGameStateHistory}
-          resetGame={resetGame}
-        />
+        <GameManager />
         <Timeline />
       </Stack>
     </ThemeProvider>
