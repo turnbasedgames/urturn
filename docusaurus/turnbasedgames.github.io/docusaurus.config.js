8625c10ec63df10cbf8a4f4a3f94bb098e158102
sarahannali <55365172+sarahannali@users.noreply.github.com>
2022-08-04 22:51:59 -0500

    FIX: Docusaurus Broken Link (#9)
    * initial titles for tutorial
    
    * add basics of getting-started
    
    * add deploying info
    
    * move files around for create-game
    
    * general outline complete
    
    * finished create-game and is working
    
    * finish tutorial
    
    * clean up understanding backend
    
    * fix code formatting
    
    * add logo
    
    * change theme
    
    * Update docs/backend.md
    
    Co-authored-by: Yoofi Quansah <yqsummon@gmail.com>
    
    * change to read-only
    
    * update finished definition
    
    * add tic-tac-toe backend comment
    
    * add admonition
    
    * add player object
    
    * add publish branch info
    
    * fix broken link
    
    Co-authored-by: Kevin <kevin26428@gmail.com>
    Co-authored-by: Yoofi Quansah <yqsummon@gmail.com>

--- docusaurus/turnbasedgames.github.io/docusaurus.config.js
+++ docusaurus/turnbasedgames.github.io/docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/getting started',
+                to: '/docs/getting-started/introduction',
               },
             ],
           },
