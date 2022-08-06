2695826cd7a41298b228030fe262870ad1121e8c
sarahannali <55365172+sarahannali@users.noreply.github.com>
2022-08-05 22:08:07 -0500

    add image (#39)

--- packages/website/src/gameView/gameList/GameList.tsx
+++ packages/website/src/gameView/gameList/GameList.tsx
@@ -16,6 +16,11 @@ const GameList = () => {
   }, []);
   const history = useHistory();
 
+  const handleImageError = (e: any) => {
+    e.target.onerror = null;
+    e.target.src = 'https://images.unsplash.com/photo-1570989614585-581ee5f7e165?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80';
+  };
+
   return (
     <Stack
       direction="column"
@@ -32,35 +37,42 @@ const GameList = () => {
           flexDirection: 'row',
         }}
       >
-        {games.map((game) => (
-          <ListItem
-            disableGutters
-            sx={{ width: '300px', paddingRight: 2 }}
-            key={`GameListItem-${game.id}`}
-          >
-            <Card // TODO: separate component
-              key={`GameCard-${game.id}`}
-              sx={{ width: '100%' }}
+        {games.map((game) => {
+          const parsedGithubURL = new URL(game.githubURL);
+          const repoOwner = parsedGithubURL.pathname.split('/')[1];
+          const repo = parsedGithubURL.pathname.split('/')[2];
+
+          return (
+            <ListItem
+              disableGutters
+              sx={{ width: '300px', paddingRight: 2 }}
+              key={`GameListItem-${game.id}`}
             >
-              <CardActionArea
-                onClick={() => history.push(`/games/${game.id}`)}
+              <Card // TODO: separate component
+                key={`GameCard-${game.id}`}
+                sx={{ width: '100%' }}
               >
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="https://images.unsplash.com/photo-1570989614585-581ee5f7e165?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                  alt={game.name}
-                />
-                <CardContent>
-                  <Typography noWrap>{game.name}</Typography>
-                  <Typography noWrap>
-                    {`by: ${game.creator.username}`}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </ListItem>
-        ))}
+                <CardActionArea
+                  onClick={() => history.push(`/games/${game.id}`)}
+                >
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={`https://rawcdn.githack.com/${repoOwner}/${repo}/published/thumbnail.png`}
+                    alt={game.name}
+                    onError={handleImageError}
+                  />
+                  <CardContent>
+                    <Typography noWrap>{game.name}</Typography>
+                    <Typography noWrap>
+                      {`by: ${game.creator.username}`}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </ListItem>
+          );
+        })}
       </List>
     </Stack>
   );
