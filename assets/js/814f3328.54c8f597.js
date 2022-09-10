81958ef9669c7bf951bb376db18dfbb1e4277247
Kevin <kevin26428@gmail.com>
2022-09-09 20:12:00 -0500

    Deploy website - based on f9619ae407fc0539d97c0d05e19896cde2734878

---
+++ assets/js/814f3328.54c8f597.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_urturn_docs=self.webpackChunk_urturn_docs||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
---
+++ assets/js/a7023ddc.99d735ed.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_urturn_docs=self.webpackChunk_urturn_docs||[]).push([[713],{3457:e=>{e.exports=JSON.parse('{"/blog/tags/welcome":{"allTagsPath":"/blog/tags","slug":"/blog/tags/welcome","name":"welcome","count":1,"permalink":"/blog/tags/welcome"}}')}}]);
