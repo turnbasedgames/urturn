46a88d32a63d5a1a78ca96e7968ccd3e64619712
Kevin <kevin26428@gmail.com>
2022-09-21 18:33:54 -0500

    fix buffer usage

--- packages/website/src/firebase/setupFirebase.ts
+++ packages/website/src/firebase/setupFirebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
+import { Buffer } from 'buffer';
 
 const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG as string;
 const firebaseApp = initializeApp(JSON.parse(Buffer.from(firebaseConfig, 'base64').toString('ascii')));
