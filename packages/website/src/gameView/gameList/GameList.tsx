092a04bcc569eeb8d375285c153a8923f10fefa1
Kevin Tang <kevin26428@gmail.com>
2022-10-23 13:19:09 -0700

    [MAIN-198] Side scroll for featured games (#31)
    * side scroll for featured games
    
    * add styling for hover and shape
    
    * finish general styling for featured and grid
    
    * add padding
    
    * remove color package
    
    * remove color
    
    * remove color from website
    
    * remove color
    
    * update featured games
    
    Co-authored-by: Sarah <asarahali00@gmail.com>

--- packages/website/src/gameView/gameList/GameList.tsx
+++ packages/website/src/gameView/gameList/GameList.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Card, CardActionArea, CardContent, Stack, Typography,
+  Stack, Typography, Grid,
 } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Game } from '@urturn/types-common';
 
-import { getGames } from '../../models/game';
-import CardMediaWithFallback from '../CardMediaWithFallback';
+import { getGame, getGames } from '../../models/game';
 import logger from '../../logger';
+import GameListCard from './GameListCard';
+
+const FEATURED_GAME_IDS = ['626eac7c65667f00160a6b42', '62adfb1b212915000e44e7a8', '62f03a69c4b031000ea00bf0', '630ebdef9495d4000ee694cb', '630af4b26c3be1000e26aca4'];
 
 function GameList(): React.ReactElement {
   const { search } = useLocation();
@@ -33,38 +34,72 @@ function GameList(): React.ReactElement {
 
     setupGames().catch(logger.error);
   }, [searchText]);
-  const navigate = useNavigate();
+
+  const [featuredGames, setFeaturedGames] = useState<Game[]>([]);
+  useEffect(() => {
+    async function setupFeaturedGames(): Promise<void> {
+      const featuredGamesRaw: Game[] = (await Promise.all(
+        FEATURED_GAME_IDS.map(async (gameId) => {
+          try {
+            return await getGame(gameId);
+          } catch (err) {
+            logger.error(err);
+          }
+          return undefined;
+        }),
+      )).filter((game): game is Game => game != null);
+      setFeaturedGames(featuredGamesRaw);
+    }
+    setupFeaturedGames().catch(logger.error);
+  }, []);
+
+  const isSearch = searchText !== '' && searchText !== undefined;
 
   return (
     <Stack
       direction="column"
-      padding={2}
-      overflow="auto"
+      padding={1}
     >
+      {
+        !isSearch && (
+          <>
+            <Typography color="text.primary">
+              Featured Games
+            </Typography>
+            <Stack
+              direction="row"
+              spacing={2}
+              paddingTop={2}
+              paddingBottom={2}
+              sx={{
+                overflowX: 'scroll',
+                flexShrink: 0,
+                '&::-webkit-scrollbar': {
+                  display: 'none',
+                },
+              }}
+            >
+              {featuredGames.map((game) => (
+                <GameListCard key={game.id} game={game} />
+              ))}
+            </Stack>
+          </>
+        )
+      }
       <Typography color="text.primary">
-        {(searchText === '' || searchText === undefined) ? 'All Games' : ''}
+        {isSearch ? '' : 'All Games'}
       </Typography>
       <Grid
         container
+        columns={2}
+        spacing={2}
+        paddingTop={2}
+        justifyContent="center"
       >
         {games.map((game) => (
-          <Card // TODO: separate component
-            sx={{ margin: 1, width: '170px' }}
-            key={`GameCard-${game.id}`}
-          >
-            <CardActionArea onClick={() => navigate(`/games/${game.id}`)}>
-              <CardMediaWithFallback
-                sx={{ height: '170px', width: '170px' }}
-                game={game}
-              />
-              <CardContent sx={{ padding: 1 }}>
-                <Typography noWrap>{game.name}</Typography>
-                <Typography noWrap variant="caption">
-                  {`by: ${game.creator.username}`}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+          <Grid key={game.id} item>
+            <GameListCard game={game} />
+          </Grid>
         ))}
       </Grid>
     </Stack>
---
+++ packages/website/src/gameView/gameList/GameListCard.tsx
@@ -0,0 +1,44 @@
+import {
+  Card, CardActionArea, CardContent, Typography,
+} from '@mui/material';
+import { Game } from '@urturn/types-common';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import CardMediaWithFallback from '../CardMediaWithFallback';
+
+interface GameListCardProps {
+  game: Game
+}
+
+function GameListCard({ game }: GameListCardProps): React.ReactElement {
+  const navigate = useNavigate();
+
+  return (
+    <Card
+      sx={{
+        maxWidth: '170px',
+        flexShrink: 0,
+        transition: '0.2s',
+        '&:hover': {
+          transform: 'scale(1.1)',
+        },
+      }}
+      key={`GameCard-${game.id}`}
+    >
+      <CardActionArea onClick={() => navigate(`/games/${game.id}`)}>
+        <CardMediaWithFallback
+          sx={{ height: '170px', width: '170px' }}
+          game={game}
+        />
+        <CardContent sx={{ padding: 1.5 }}>
+          <Typography variant="h6" noWrap>{game.name}</Typography>
+          <Typography color="text.secondary" noWrap variant="caption">
+            {`by: ${game.creator.username}`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default GameListCard;
