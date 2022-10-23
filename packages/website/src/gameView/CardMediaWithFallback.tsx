a1a599d3a10c75162a82c332768c707f1c401692
Sarah <asarahali00@gmail.com>
2022-10-22 18:27:48 -0700

    remove color

--- packages/website/src/gameView/CardMediaWithFallback.tsx
+++ packages/website/src/gameView/CardMediaWithFallback.tsx
@@ -16,7 +16,7 @@ function CardMediaWithFallback({ game, sx }: { game: Game, sx: SxProps }): React
     <CardMedia
       sx={sx}
       component="img"
-      image={`https://rawcdn.githack.com/${repoOwner}/${repo}/${game.commitSHA}/thumbnail.png`}
+      image="https://assets-prd.ignimgs.com/2022/06/23/overwatch-2-button-fin-1656022954568.jpg"
       alt={game.name}
       onError={handleImageError}
     />
