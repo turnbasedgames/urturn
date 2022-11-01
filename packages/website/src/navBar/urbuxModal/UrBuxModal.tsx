2983ea9bfb2c0f1be0fde53573d8bc0b16bba85e
Yoofi Quansah <yqsummon@gmail.com>
2022-10-31 21:01:40 -0700

    chore: 100 urbux, 1000 urbux (#51)
    * chore: 100 urbux, 1000 urbux
    
    * chore: change text to account for 1000 urbux change
    
    * chore: fix up last message

--- packages/website/src/navBar/urbuxModal/UrBuxModal.tsx
+++ packages/website/src/navBar/urbuxModal/UrBuxModal.tsx
@@ -24,7 +24,7 @@ function UrBuxModal({ open, setOpen }: UrBuxProps): React.ReactElement {
       {intent
         ? (
           <Card sx={{ padding: 2 }}>
-            <CardHeader title="Checkout" titleTypographyProps={{ color: 'secondary' }} subheader="Buying 100 UrBux for $1 USD" sx={{ padding: 0, marginBottom: 1 }} />
+            <CardHeader title="Checkout" titleTypographyProps={{ color: 'secondary' }} subheader="Buying 1000 UrBux for $10 USD" sx={{ padding: 0, marginBottom: 1 }} />
             <PaymentCard />
           </Card>
         )
@@ -38,7 +38,7 @@ function UrBuxModal({ open, setOpen }: UrBuxProps): React.ReactElement {
             />
             <CardActions>
               <Button startIcon={<GiTwoCoins />} onClick={() => setIntent(true)}>
-                100 UrBux for $1
+                1000 UrBux for $10
               </Button>
             </CardActions>
           </Card>
