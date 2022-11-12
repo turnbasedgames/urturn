48ae8b74a2ddad507c9f8935da64da82aa778987
Kevin Tang <kevin26428@gmail.com>
2022-11-11 20:49:03 -0800

    fix linting

--- packages/ui-common/src/components/RoomPlayer/PlayerMenu.tsx
+++ packages/ui-common/src/components/RoomPlayer/PlayerMenu.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {
-  IconButton, Paper, Stack, Modal, Card, Typography, CardContent, CardActions, Button, CardHeader, Tooltip,
+  IconButton, Paper, Stack, Modal, Card, Typography, CardContent, CardActions, Button, CardHeader,
+  Tooltip,
 } from '@mui/material';
 import { SiDiscord } from 'react-icons/si';
 import { ThemeProvider } from '@mui/material/styles';
