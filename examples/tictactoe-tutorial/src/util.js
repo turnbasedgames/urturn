7cebc1ac1b56ab17da4cedf13af88e1f1beead50
Kevin <kevin26428@gmail.com>
2022-11-18 11:40:28 -0800

    add tictactoe tutorial base

---
+++ examples/tictactoe-tutorial/src/util.js
@@ -0,0 +1,34 @@
+export const Status = Object.freeze({
+  PreGame: 'preGame',
+  InGame: 'inGame',
+  EndGame: 'endGame',
+});
+
+export const Mark = Object.freeze({
+  X: 'X',
+  O: 'O',
+});
+
+/**
+ * evaluateBoard() determines if the tictactoe game is finished and provides details (tie or winner)
+ * @param {string[][]} board, a 3x3 2D array with 'X' and 'O' as values
+ * @param {{[string]: string}} plrIdToPlrMark, map from plrId to the player's mark
+ * @param {Player[]} players, list of players
+ * @returns {
+ *  winner?: Player, (the player that won the game if they exist)
+ *  tie?: bool, (true if tie, and false if not a tie)
+ *  finished: bool, (true if the game is finished, and false if not finished)
+ * }
+ */
+export const evaluateBoard = (board, plrIdToPlrMark, players) => {
+  // TODO: check for tie and return correct result
+
+  /**
+   * TODO: check for a winner
+   * - check rows
+   * - check columns
+   * - check diagonals
+   */
+
+  // TODO: return default not finished
+};
