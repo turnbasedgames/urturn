c0a8e8407ba4105c0c9dbd433f958a133d2a5ad8
Kevin <kevin26428@gmail.com>
2022-11-20 13:55:51 -0800

    prepare pulling common room logic from runner and server

---
+++ packages/runner-core/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "xo",
+    "overrides": [
+        {
+            "extends": [
+                "xo-typescript"
+            ],
+            "files": [
+                "*.ts",
+                "*.tsx"
+            ]
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
---
+++ packages/runner-core/README.md
@@ -0,0 +1,11 @@
+# `runner-core`
+
+> TODO: description
+
+## Usage
+
+```
+const runnerCore = require('runner-core');
+
+// TODO: DEMONSTRATE API
+```
