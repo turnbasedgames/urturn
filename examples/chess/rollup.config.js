e69eb9554c34aa24a41b0503f3947261a43ad7a3
Kevin <kevin26428@gmail.com>
2022-11-22 21:34:59 -0800

    add workflow for chess

--- examples/chess/rollup.config.js
+++ examples/chess/rollup.config.js
@@ -1,18 +1,12 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
 const pkg = require('./package.json');
 
-module.exports = [
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/main.js',
-    external: ['ms'],
-    output: [
-      { file: pkg.main, format: 'cjs' },
-    ],
-  },
-];
+module.exports = {
+  input: 'src/main.js',
+  external: ['ms'],
+  output: [
+    { file: pkg.main, format: 'cjs' },
+  ],
+  plugins: [resolve(), commonjs()],
+};
