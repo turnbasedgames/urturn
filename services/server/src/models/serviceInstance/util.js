28e3a49e46a9f96a0121e6bcd425d17fc92c8596
Kevin <kevin26428@gmail.com>
2022-11-28 20:35:38 -0800

    handle potential errors

--- services/server/src/models/serviceInstance/util.js
+++ services/server/src/models/serviceInstance/util.js
@@ -1,10 +1,20 @@
 const ServiceInstance = require('./serviceInstance');
+const logger = require('../../logger');
 
 // how frequently to update the ping count for a service instance
 const PING_INTERVAL_MS = 10000;
 
 async function startServiceInstancePingChain(instId) {
-  await ServiceInstance.findByIdAndUpdate(instId, { $inc: { pingCount: 1 } });
+  try {
+    const result = await ServiceInstance.findByIdAndUpdate(
+      instId,
+      { $inc: { pingCount: 1 } },
+      { rawResult: true },
+    );
+    logger.info('Result of updating serviceInstance pingCount', result.lastErrorObject);
+  } catch (error) {
+    logger.error('Error when trying to update pingCount for serviceInstance!', error);
+  }
   setTimeout(() => {
     startServiceInstancePingChain(instId);
   }, PING_INTERVAL_MS);
