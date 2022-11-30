69d0efcad684f8ab7bf5a9417f36cf51e42661f3
Kevin <kevin26428@gmail.com>
2022-11-29 20:17:52 -0800

    log whenever we are not using default application credentials

--- services/server/src/setupFirebase.js
+++ services/server/src/setupFirebase.js
@@ -1,7 +1,17 @@
 const admin = require('firebase-admin');
+const logger = require('./logger');
 
-admin.initializeApp({
-  credential: process.env.GOOGLE_APPLICATION_CREDENTIALS
-    ? admin.credential.applicationDefault()
-    : admin.credential.cert(JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString('ascii'))),
-});
+let credential;
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS != null) {
+  logger.info('Using default Google Application Credentials credentials');
+  credential = admin.credential.applicationDefault();
+} else if (process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64 != null) {
+  logger.warn('Getting Google Application Credentials as an environment variable: GOOGLE_APPLICATION_CREDENTIALS_BASE64');
+  logger.warn('This should never be done in production!');
+  credential = admin.credential.cert(JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString('ascii')));
+} else {
+  logger.fatal('No Google Application Credential detected for service! It is required for the service to work properly!');
+  throw new Error('No firebase credential detected for service!');
+}
+
+admin.initializeApp({ credential });
