7713ec81ba3745df8da5d26c06f0122bad7c271a
Sarah <asarahali00@gmail.com>
2022-12-02 22:34:59 -0800

    update to link instead of modal

--- packages/ui-common/src/components/RoomPlayer/PlayerMenu.tsx
+++ packages/ui-common/src/components/RoomPlayer/PlayerMenu.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import {
-  IconButton, Paper, Stack, Modal, Card, Typography, CardContent, CardActions, Button, CardHeader,
+  IconButton, Paper, Stack, Modal, Card, CardHeader,
   Tooltip,
 } from '@mui/material';
-import { SiDiscord } from 'react-icons/si';
 import { ThemeProvider } from '@mui/material/styles';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { TbLetterU } from 'react-icons/tb';
-import { DISCORD_URL, DOCS_URL, RoomUser } from '@urturn/types-common';
+import { RoomUser } from '@urturn/types-common';
 import PeopleIcon from '@mui/icons-material/People';
 import logger from '../../logger';
 import Theme from '../Theme';
@@ -19,41 +18,10 @@ interface PlayerMenuProps {
 }
 
 function PlayerMenu({ quitRoom, players, curPlayer }: PlayerMenuProps): React.ReactElement {
-  const [aboutModalOpen, setAboutModalOpen] = useState(false);
   const [playersModalOpen, setPlayersModalOpen] = useState(false);
   const spectating = players.every(({ id }) => id !== curPlayer.id);
   return (
     <ThemeProvider theme={Theme}>
-      <Modal
-        open={aboutModalOpen}
-        onClose={() => setAboutModalOpen(false)}
-        style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-      >
-        <Card sx={{ maxWidth: '400px' }}>
-          <CardHeader
-            title="UrTurn"
-            subheader="Where you make and play games"
-            action={(
-              <IconButton
-                href={DISCORD_URL}
-                target="_blank"
-              >
-                <SiDiscord />
-              </IconButton>
-            )}
-            sx={{ paddingBottom: 0.5 }}
-          />
-          <CardContent>
-            <Typography variant="body2">
-              UrTurn handles all the backend infrastructure for hosting and monetizing
-              your game so you can make games just like this one.
-            </Typography>
-          </CardContent>
-          <CardActions sx={{ justifyContent: 'flex-end' }}>
-            <Button size="small" href={DOCS_URL} target="_blank">Start Building</Button>
-          </CardActions>
-        </Card>
-      </Modal>
       <Modal
         open={playersModalOpen}
         onClose={() => setPlayersModalOpen(false)}
@@ -108,9 +76,9 @@ function PlayerMenu({ quitRoom, players, curPlayer }: PlayerMenuProps): React.Re
             <IconButton
               size="small"
               sx={{ borderRadius: 1 }}
-              onClick={() => {
-                setAboutModalOpen(!aboutModalOpen);
-              }}
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <TbLetterU />
             </IconButton>
