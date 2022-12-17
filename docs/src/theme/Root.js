08a7ea337750933a3ae918a2ce5f9432bc7faf54
Kevin <kevin26428@gmail.com>
2022-12-16 18:04:52 -0800

    setup ephemeral user

--- docs/src/theme/Root.js
+++ docs/src/theme/Root.js
@@ -1,13 +1,25 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Tracker from '@openreplay/tracker/cjs';
+import { v4 as uuidv4 } from 'uuid';
 
 // Swizzle root component to persist tracker across pages
 const tracker = new Tracker({
   projectKey: "g9SNLNQNtzt4vmVLgzTs",
   onStart: ({ sessionID }) => {
-    console.log("OpenReplay session started", sessionID)
+    // create randomly generated uuid and store in local storage
+    let ephemeralUserId = localStorage.getItem('ephemeralUserId');
+    if (ephemeralUserId == null) {
+      let ephemeralUserId = uuidv4();
+      localStorage.setItem('ephemeralUserId', ephemeralUserId);
+    }
+
+    tracker.setMetadata('sessionID', sessionID)
+    tracker.setUserAnonymousID(ephemeralUserId);
+
+    console.log("OpenReplay session started", { sessionID, ephemeralUserId })
     if (window.gtag != null) {
       window.gtag('set', {
+        'user_id': ephemeralUserId,
         'open_replay_session_id': sessionID,
       });
     } else {
