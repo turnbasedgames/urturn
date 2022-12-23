4dd9cc4ccc1ee3a032aedcd0d7810f988cb76f6e
Kevin <kevin26428@gmail.com>
2022-12-23 13:39:46 -0800

    add pixi template

--- templates/template-pixi/src/index.js
+++ templates/template-pixi/src/index.js
@@ -1,19 +1,32 @@
-import { Application, Sprite } from 'pixi.js'
+import client from '@urturn/client';
+import { Application, Sprite, Text } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas"),
 	resolution: window.devicePixelRatio || 1,
+	resizeTo: window,
 	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	backgroundColor: 0x000000,
 });
 
-const clampy = Sprite.from("assets/urturnIcon.png");
+const icon = Sprite.from("assets/urturnIcon.png");
 
-clampy.anchor.set(0.5);
+icon.anchor.set(0.5);
+icon.x = app.screen.width / 2;
+icon.y = app.screen.height / 2 + 50;
+icon.height = 50;
+icon.width = 50;
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+const basicText = new Text('TODO: Make the frontend of your game!', {
+	fill: 'white'
+});
+basicText.anchor.set(0.5);
+basicText.x = app.screen.width / 2;
+basicText.y = app.screen.height / 2;
 
-app.stage.addChild(clampy);
+app.stage.addChild(icon);
+app.stage.addChild(basicText);
+
+client.events.on('stateChanged', (newRoomState) => {
+	console.log('new roomState:', newRoomState);
+});
