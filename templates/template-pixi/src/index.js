8406beb30a2235a74fb8fabe7c388554b75fca41
Kevin <kevin26428@gmail.com>
2022-12-23 12:51:21 -0800

    add template pixi

---
+++ templates/template-pixi/src/index.js
@@ -0,0 +1,19 @@
+import { Application, Sprite } from 'pixi.js'
+
+const app = new Application({
+	view: document.getElementById("pixi-canvas"),
+	resolution: window.devicePixelRatio || 1,
+	autoDensity: true,
+	backgroundColor: 0x6495ed,
+	width: 640,
+	height: 480
+});
+
+const clampy = Sprite.from("assets/urturnIcon.png");
+
+clampy.anchor.set(0.5);
+
+clampy.x = app.screen.width / 2;
+clampy.y = app.screen.height / 2;
+
+app.stage.addChild(clampy);
