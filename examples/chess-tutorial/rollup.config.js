1345e7a2b11614a5214df29045711b069450f49c
Kevin Tang <kevin26428@gmail.com>
2023-01-05 14:11:29 -0800

    runner uses source mapping (#205)

--- examples/chess-tutorial/rollup.config.js
+++ examples/chess-tutorial/rollup.config.js
@@ -2,11 +2,18 @@ const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const pkg = require('./package.json');
 
-module.exports = {
-  input: 'src/main.js',
-  external: ['ms'],
-  output: [
-    { file: pkg.main, format: 'cjs' },
-  ],
-  plugins: [resolve(), commonjs()],
-};
+module.exports = [
+  {
+    input: 'src/main.js',
+    external: ['ms'],
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      // @urturn/runner uses the sourcemap to provide correct error traces
+      sourcemap: true,
+    },
+    // plugins that allow the src/main.js file require various npm packages
+    // WARNING: packages that access the network, disk/file system will not work.
+    plugins: [resolve(), commonjs()],
+  },
+];
