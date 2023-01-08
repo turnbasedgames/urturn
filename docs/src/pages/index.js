d704a238752fddd4a31855d8af0037c36768293f
Kevin <kevin26428@gmail.com>
2023-01-08 01:48:36 -0800

    cleanup splash page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,54 +1,64 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import { Stack, Slide, Fade } from '@mui/material'
+import GitHubButton from 'react-github-btn'
+import { Stack, Slide, Fade, Box } from '@mui/material'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import ShowCase from '../components/ShowCase';
 import Link from '@docusaurus/Link';
+import RunnerSvg from './runner-init.svg'
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero', styles.heroBanner)}>
-      <Stack margin="auto" width="70%" justifyContent="center" alignItems="center"
-        sx={{ maxWidth: "1000px" }}
-      >
-        <Slide direction="up" in timeout={200}>
-          <Stack>
-            <Fade in timeout={1000}>
-              <h1 className="hero__title">
-                <a className="hero__hard-title">
-                  {siteConfig.title.split(' ').slice(0, 3).join(' ')}
-                </a>
-                {' ' + siteConfig.title.split(' ').slice(3).join(' ')}
-              </h1>
-            </Fade>
-          </Stack>
-        </Slide>
-        <Slide direction="up" in timeout={500}>
-          <Stack justifyContent="center" alignItems="center">
-            <Fade in timeout={1000}>
-              <p className="hero__subtitle">{siteConfig.tagline}</p>
-            </Fade>
-            <Slide direction='up' in timeout={1000}>
-              <Stack direction="row" padding={2} spacing={2}>
-                <Link
-                  className="button button--lg button--primary button--primary"
-                  to="/docs/category/getting-started"
-                >
-                  Get Started
-                </Link>
-                <Link
-                  className="button button--lg button--outline button--secondary"
-                  href="/docs"
-                >
-                  Learn More
-                </Link>
-              </Stack>
-            </Slide>
-          </Stack>
-        </Slide>
+      <Stack width="100%" direction="row" spacing={2} padding={2} justifyContent="space-around">
+        <Stack justifyContent="flex-start" alignItems="flex-start"
+          sx={{ maxWidth: "600px" }}
+        >
+          <Slide direction="up" in timeout={200} alignItems="flex-start">
+            <Stack alignItems="flex-start">
+              <Fade in timeout={1000}>
+                <h1 className="hero__title">
+                  <a className="hero__hard-title">
+                    {siteConfig.title.split(' ').slice(0, 3).join(' ')}
+                  </a>
+                  <br />
+                  {' ' + siteConfig.title.split(' ').slice(3).join(' ')}
+                </h1>
+              </Fade>
+            </Stack>
+          </Slide>
+          <Slide direction="up" in timeout={500}>
+            <Stack justifyContent="space-between" alignItems="flex-start">
+              <Fade in timeout={1000}>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+              </Fade>
+              <Slide direction='up' in timeout={1000} alignItems="flex-start">
+                <Stack direction="row" spacing={2} justifyContent="flex-start" alignItems="center">
+                  <Link
+                    className="button button--lg button--primary button--primary"
+                    to="/docs/category/getting-started"
+                  >
+                    Get Started
+                  </Link>
+                  <Link
+                    className="button button--lg button--outline button--secondary"
+                    href="/docs"
+                  >
+                    Learn More
+                  </Link>
+                  <Box />
+                  <GitHubButton data-icon='octoicon-star' data-color-scheme='light_high_contrast' data-size='large' data-show-count href="https://github.com/turnbasedgames/urturn">
+                    Star us on Github! ✨
+                  </GitHubButton>
+                </Stack>
+              </Slide>
+            </Stack>
+          </Slide>
+        </Stack>
+        <RunnerSvg />
       </Stack>
     </header>
   );
--- docs/src/theme/Root.js
+++ docs/src/theme/Root.js
@@ -1,11 +1,8 @@
 import React, { useEffect } from 'react';
-import Tracker from '@openreplay/tracker/cjs';
 import { v4 as uuidv4 } from 'uuid';
 
-// Swizzle root component to persist tracker across pages
-const tracker = new Tracker({
-  projectKey: "g9SNLNQNtzt4vmVLgzTs",
-  onStart: ({ sessionID }) => {
+export default function Root({ children }) {
+  useEffect(() => {
     // create randomly generated uuid and store in local storage
     let ephemeralUserId = localStorage.getItem('ephemeralUserId');
     if (ephemeralUserId == null) {
@@ -13,10 +10,6 @@ const tracker = new Tracker({
       localStorage.setItem('ephemeralUserId', ephemeralUserId);
     }
 
-    tracker.setMetadata('sessionID', sessionID)
-    tracker.setUserAnonymousID(ephemeralUserId);
-
-    console.log("OpenReplay session started", { sessionID, ephemeralUserId })
     if (window.gtag != null) {
       window.gtag('set', {
         'user_id': ephemeralUserId,
@@ -25,12 +18,6 @@ const tracker = new Tracker({
     } else {
       console.warn('was not able to configure session id in gtag!')
     }
-  },
-});
-
-export default function Root({ children }) {
-  useEffect(() => {
-    tracker.start();
   }, []);
 
   return <>{children}</>;
